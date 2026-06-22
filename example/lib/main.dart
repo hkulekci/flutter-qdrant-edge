785344c99e3e15fd9b7b5704c65c8cd0362ce3e9
Haydar KULEKCI <haydarkulekci@gmail.com>
2026-06-22 08:59:40 +0300

    Remove unused dart:io import from example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,8 +4,6 @@
 //   sh ../scripts/build-ios.sh      (or build-android.sh)
 //   flutter run
 
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:qdrant_edge_flutter/qdrant_edge_flutter.dart';
